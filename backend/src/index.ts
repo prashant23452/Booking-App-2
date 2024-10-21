9ff81d3b11bb0f69343d9b683dd15791a501795c
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-10-21 15:55:17 +0530

    132-backend register & login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -12,10 +14,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 
-app.get("/api/test", async (req:Request, res:Response)=>{
-   res.json({message: "hello from express endpoints"});
-})
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, ()=>{
-    console.log("Server is running on localhost:7000");
+    console.log("Server is running on localhost:3000");
 })
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,50 @@
+import express, {Request, Response} from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+const router= express.Router();
+
+// /api/users/register
+router.post("/register", [
+    check("firstName", "First Name is Requied").isString(),
+    check("lastName", "Last Name is Requied").isString(),
+    check("email", "Email is Requied").isEmail(),
+    check("password", "Password with six or more characters  is Requied").isLength({min:6}),
+],async (req:Request, res:Response): Promise<void> => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({message: errors.array()});
+        return ;
+    }
+   try{
+       let user = await User.findOne({
+         email: req.body.email,
+       });
+       if(user){
+        res.status(400).json({ mesaage: "User already exist" });
+        return;
+       }
+       user=new User(req.body);
+       await user.save();
+
+       const token=jwt.sign({userId: user.id}, 
+        process.env.JWT_SECRET_KEY as string,{
+            expiresIn: "1d"
+        }
+    );
+    res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000
+    })
+   res.sendStatus(200); 
+   return;
+   }
+   catch(error){
+      console.log(error);
+      res.status(500).send({message: "Something went wrong"});
+   }
+})
+
+export default router;
